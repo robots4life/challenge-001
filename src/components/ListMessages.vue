759adf1c12e07edca46664cdafc970815be2fb4b
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-08 18:22:42 +0200

    get single message url

--- src/components/ListMessages.vue
+++ src/components/ListMessages.vue
@@ -14,23 +14,6 @@ async function getAllMessages() {
 function removeAllMessages() {
 	return (messages.value = '');
 }
-//
-// by doing getAllMessages we already get all the date for each message
-// so having a query to get a single message data is redundant and only extra traffic to the API !
-//
-// async function getSingleMessageDetails(id) {
-// 	const response = await fetch(`http://localhost:3030/messages/${id}`);
-// 	singleMessageDetails.value = await response.json();
-// 	console.log(singleMessageDetails.value);
-// }
-//
-// solution: use the v-show directive ?
-// https://vuejs.org/guide/essentials/conditional.html#v-show
-/* "The difference is that an element with v-show will always be rendered and
-	remain in the DOM; v-show only toggles the display CSS property of the element."
-*/
-
-let showMessageDetails = false;
 </script>
 
 <template>
@@ -43,18 +26,9 @@ let showMessageDetails = false;
 	<hr />
 	<ul v-for="message in messages" :key="message.id">
 		<li>
-			<p>{{ message.type }} : {{ message.id }}</p>
-			<!-- don't do this below -->
-			<!-- <button @click="getSingleMessageDetails(message.id)">Show Message Details</button> -->
-			<!-- {{ singleMessageDetails }} -->
-
-			<!-- instead use the v-show directive -->
-			<button @click="showMessageDetails = !showMessageDetails">Show Message Details</button>
-			<!-- but this also does not work as expected -->
-			<div v-show="showMessageDetails">
-				<p>{{ message.properties }}</p>
-			</div>
-			<a target="_blank" :href="message.properties.url">{{ message.properties.url }}</a>
+			<p>{{ message.type }}</p>
+			<a :href="'http://localhost:3030/messages/' + message.id">Details</a>
+			<!-- <p>{{ message.properties }}</p> -->
 		</li>
 		<hr />
 	</ul>
