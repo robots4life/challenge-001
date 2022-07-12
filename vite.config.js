bf79f5fb68aeaa1cfabd99d0ea0e54eb5b8b1d78
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-12 17:28:54 +0200

    vite remote container devilbox setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [vue()],
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		strictPort: true,
+		hmr: {
+			clientPort: 443
+		}
+	},
+	define: {
+		// env-cmd https://blog.hdks.org/Environment-Variables-in-SvelteKit-and-Vercel/
+		'process.env': process.env
+	}
+});
