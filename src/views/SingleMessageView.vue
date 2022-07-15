312364c3088d3545c5cec63531fadcd30fbee38c
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-15 18:27:32 +0200

    get route parameter id with useRoute

--- src/views/SingleMessageView.vue
+++ src/views/SingleMessageView.vue
@@ -6,6 +6,13 @@
 	</div>
 </template>
 
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
 <style scoped>
 .single-message {
 	text-align: center;
