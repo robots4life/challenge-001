8fea753e2446f5f54ee0b66ca26f41836dee4f6e
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-15 18:39:29 +0200

    load single message details

--- src/views/SingleMessageView.vue
+++ src/views/SingleMessageView.vue
@@ -3,14 +3,30 @@
 		<h1>Single Message Details</h1>
 		<h2>Case : {{ $route.params.id }}</h2>
 		<hr />
+		<p>{{ messageDetails }}</p>
 	</div>
 </template>
 
 <script setup>
+import { onBeforeMount, ref } from 'vue';
 import { useRoute } from 'vue-router';
+let messageDetails = ref(null);
 
 const route = useRoute();
 console.log(route.params.id);
+
+async function getSingleMessageDetails() {
+	try {
+		const response = await fetch(`http://localhost:3030/messages/${route.params.id}`);
+		messageDetails.value = await response.json();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+onBeforeMount(() => {
+	getSingleMessageDetails();
+});
 </script>
 
 <style scoped>
